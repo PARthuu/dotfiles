a6118aa8fba639b3470fb7b0177b7c4fbefdea4f
PARthuu <parthivmaneoti@gmail.com>
2026-03-16 23:38:18 +0530

    added telescope support in harpoon

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,15 +4,40 @@ local harpoon = require 'harpoon'
 harpoon:setup()
 -- REQUIRED
 
+-- Use Telescope to search the files
+local conf = require('telescope.config').values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require('telescope.pickers')
+        .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+                results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+        })
+        :find()
+end
+
 -- Add current file to Harpoon list
 vim.keymap.set('n', '<leader>a', function()
     harpoon:list():add()
 end, { desc = 'Harpoon: Add current file to list' })
 
--- Toggle Harpoon quick menu UI
+-- Toggle Harpoon Telescope quick menu UI
 vim.keymap.set('n', '<M-e>', function()
+    toggle_telescope(harpoon:list())
+end, { desc = 'Open harpoon telescope window' })
+
+-- Toggle Harpoon quick menu UI
+vim.keymap.set('n', '<leader>s,', function()
     harpoon.ui:toggle_quick_menu(harpoon:list())
-end, { desc = 'Harpoon: Toggle quick menu' })
+end, { desc = 'Open harpoon window' })
 
 -- Navigate to Harpoon file 1
 vim.keymap.set('n', '<M-1>', function()
