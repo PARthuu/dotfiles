420bbc7e904335023dcae01b2b95b3adcd5235f4
PARthuu <parthivmaneoti@gmail.com>
2026-04-22 10:03:31 +0530

    Updated: using func.lua for functions

--- nvim/lua/parthu/init.lua
+++ nvim/lua/parthu/init.lua
@@ -1,3 +1,4 @@
 require 'parthu.lazy_init'
 require 'parthu.set'
+require 'parthu.func'
 require 'parthu.remap'
--- nvim/lua/parthu/set.lua
+++ nvim/lua/parthu/set.lua
@@ -36,35 +36,3 @@ vim.o.inccommand = 'split'
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-
-vim.api.nvim_create_user_command('GitBlameLine', function()
-    local line_number = vim.fn.line '.' -- Get the current line number. See `:h line()`
-    local filename = vim.api.nvim_buf_get_name(0)
-    print(vim.fn.system { 'git', 'blame', '-L', line_number .. ',+1', filename })
-end, { desc = 'Print the git blame for the current line' })
-
-vim.schedule(function()
-    vim.o.clipboard = 'unnamedplus'
-end)
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.hl.on_yank()
-    end,
-})
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    callback = function()
-        vim.hl.on_yank()
-    end,
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'markdown', 'md' },
-    callback = function()
-        vim.lsp.enable 'marksman'
-    end,
-})
